5e0650e22a6305d9e82547a3b2940d6894a171ca
mark <markogujiuba@gmail.com>
2024-09-09 19:08:53 +0100

    javascript lesson

---
+++ learning.js
@@ -0,0 +1,38 @@
+var test1 = 'gut'; // a string
+var test2 = 123; //inter
+
+var length = 4;
+var breath = 10;
+
+// find area of a rectangle.
+// We did arithmethic operators
+var area = 2 / 4;
+
+var test;
+
+test = '234';
+
+var check = false;
+
+
+
+// concatination
+var summation = 'my' + test1; //my test 
+
+// condition is meet
+if (length != breath) {
+    console.log('yes it is same')
+} else {
+    console.log('no it is not same')
+}
+
+
+var listString = ['funmi', 'david', 'conqueror', 'mark']
+
+
+
+console.log(listString[0]);
+
+
+
+// print(listString[2]);
